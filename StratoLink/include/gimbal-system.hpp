e80d860c39bf4321f42001ae13afe19252716f78
acarrou <dridriswim@gmail.com>
2023-07-03 21:26:32 -0700

    :hammer: adds second pitch motor for current design

--- StratoLink/include/gimbal-system.hpp
+++ StratoLink/include/gimbal-system.hpp
@@ -12,9 +12,12 @@ namespace StratoLink
         const int XdirPin = 3;
         const int YstepPin = 4;
         const int YdirPin = 5;
+        const int AstepPin = 6;
+        const int AdirPin = 7;
 
         AccelStepper motor_rotation = AccelStepper(AccelStepper::DRIVER, XstepPin, XdirPin);
-        AccelStepper motor_pitch = AccelStepper(AccelStepper::DRIVER, YstepPin, YdirPin);
+        AccelStepper motor_pitch1 = AccelStepper(AccelStepper::DRIVER, YstepPin, YdirPin);
+        AccelStepper motor_pitch2 = AccelStepper(AccelStepper::DRIVER, AstepPin, AdirPin);
 
 
     public:
@@ -22,8 +25,10 @@ namespace StratoLink
         {
             motor_rotation.setMaxSpeed(200);
             motor_rotation.setAcceleration(100);
-            motor_pitch.setMaxSpeed(200);
-            motor_pitch.setAcceleration(100);
+            motor_pitch1.setMaxSpeed(200);
+            motor_pitch1.setAcceleration(100);
+            motor_pitch2.setMaxSpeed(200);
+            motor_pitch2.setAcceleration(100);
         }
 
         void SetGimbalAngles(double x, double y, double z)
@@ -46,13 +51,15 @@ namespace StratoLink
         void RunGimbalMotorAngles(double azimuth_degrees, double elevation_degrees)
         {
             motor_rotation.moveTo(azimuth_degrees);
-            motor_pitch.moveTo(elevation_degrees);
+            motor_pitch1.moveTo(elevation_degrees);
+            motor_pitch2.moveTo(elevation_degrees);
 
             // Run the motors until they reach their target positions
-            while (motor_rotation.isRunning() || motor_pitch.isRunning())
+            while (motor_rotation.isRunning() || motor_pitch1.isRunning() && motor_pitch2.isRunning())
             {
                 motor_rotation.run();
-                motor_pitch.run();
+                motor_pitch1.run();
+                motor_pitch2.run();
             }
         }
     };
